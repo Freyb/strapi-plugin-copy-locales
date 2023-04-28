ec42f70222a80a189258bc360247e9a01878541c
Freyb <balazs.frey@gmail.com>
2023-04-28 13:33:57 +0200

    Implement entry overwriting

--- server/services/generate-service.js
+++ server/services/generate-service.js
@@ -1,42 +1,75 @@
 'use strict';
 
+const { ApplicationError } = require('@strapi/utils').errors;
+
 module.exports = ({ strapi }) => {
   const getLocaleService = () => strapi.plugin('i18n').service('locales');
   const getLocalizationService = () =>
     strapi.plugin('i18n').service('localizations');
   const getEntityAPI = (uid) => strapi.query(uid);
 
-  const generate = async ({ contentType, data }) => {
+  const generate = async ({ contentType, data, locales: selectedLocales }) => {
     const localeService = getLocaleService();
     const localizationService = getLocalizationService();
     const entityAPI = getEntityAPI(contentType.uid);
 
-    const locales = await localeService.find();
+    const allLocales = (await localeService.find()).map((l) => l.code);
+    const invalidLocales = selectedLocales.some((l) => !allLocales.includes(l));
+    if (invalidLocales.length > 0) {
+      throw new ApplicationError('Invalid locales! ' + invalidLocales);
+    }
+    const {
+      id,
+      locale: originalLocale,
+      localizations = [],
+      createdBy,
+      updatedBy,
+      ...rest
+    } = data;
 
-    const originalLocale = data.locale;
-    const createdById = data.createdBy.id;
-    const updatedById = data.updatedBy.id;
-    let id = data.id;
-    const createdIds = [id];
+    const existingLocales = localizations.map((l) => l.locale);
+    const createdById = createdBy.id;
+    const updatedById = updatedBy.id;
+    const createdIds = [id, ...localizations.map((l) => l.id)];
 
-    for (let locale of locales.map((l) => l.code)) {
+    for (let locale of selectedLocales) {
       if (locale === originalLocale) continue;
 
-      const newData = {
-        ...data,
-        id: undefined,
-        createdBy: createdById,
-        updatedBy: updatedById,
-        locale,
-        localizations: createdIds,
-      };
+      if (existingLocales.includes(locale)) {
+        //Update
+        const updatedEntryID = localizations.find(
+          (l) => l.locale === locale,
+        ).id;
+        const newData = {
+          ...rest,
+          createdBy: createdById,
+          updatedBy: updatedById,
+        };
 
-      const result = await entityAPI.create({ data: newData, populate: true });
-      await localizationService.syncLocalizations(result, {
-        model: contentType,
-      });
-      id = result.id;
-      createdIds.push(id);
+        await entityAPI.update({
+          where: { id: updatedEntryID },
+          data: newData,
+          populate: true,
+        });
+      } else {
+        //Create
+        const newData = {
+          ...rest,
+          createdBy: createdById,
+          updatedBy: updatedById,
+          locale,
+          localizations: createdIds,
+        };
+
+        const result = await entityAPI.create({
+          data: newData,
+          populate: true,
+        });
+        await localizationService.syncLocalizations(result, {
+          model: contentType,
+        });
+        createdIds.push(result.id);
+      }
     }
     return { data: createdIds };
   };
